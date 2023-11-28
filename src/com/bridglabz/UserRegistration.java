dbc58e4b5125da25eae88f4bfb6fd30d46a26322
Zubair <zubairyaqoob6988@gmail.com>
2023-11-28 17:14:57 +0530

    Added: Validate Email I'D

--- src/com/bridglabz/UserRegistration.java
+++ src/com/bridglabz/UserRegistration.java
@@ -6,12 +6,13 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     Scanner sc=new Scanner(System.in);
+//    For First name
     public void firstName(){
         System.out.println("Enter First Name:");
         String fname=sc.next();
 
 
-        Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");//. represents single character
+        Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
         Matcher m = p.matcher(fname);
         if (m.matches()) {
             System.out.println("Valid Input");
@@ -20,12 +21,13 @@ public class UserRegistration {
             System.out.println("Invalid Input");
         }
     }
+//    For Second name
     public void lastName(){
         System.out.println("Enter Last Name:");
         String lname=sc.next();
 
 
-        Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");//. represents single character
+        Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
         Matcher m = p.matcher(lname);
         if (m.matches()) {
             System.out.println("Valid Input");
@@ -33,13 +35,28 @@ public class UserRegistration {
         else{
             System.out.println("Invalid Input");
         }
+    }
 
+    public void email(){
+        System.out.println("Enter your Email:");
+        String email = sc.next();
+
+        Pattern p = Pattern.compile("^[a][b][c][.][a-z]{1,10}@[b][l][.][c][o][.][a-z]{1,10}$");
+        Matcher m = p.matcher(email);
+
+        if(m.matches()){
+            System.out.println("Valid Email");
+        }
+        else{
+            System.out.println("Invalid Email");
+        }
     }
     public static void main(String[] args) {
         System.out.println("WELCOME TO USER REGISTRATION!");
         UserRegistration userRegistration=new UserRegistration();
         userRegistration.firstName();
         userRegistration.lastName();
+        userRegistration.email();
 
     }
 }
